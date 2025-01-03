b89180d44ba04710050915b2f684e51b638c8adc
stantonik <stanleyy.arnaud@gmail.com>
2025-01-03 16:12:44 +0100

    edit readme and example

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # csimple-ecs
-A simple Entity-Component-System in pure C99.
+
+> A simple Entity-Component-System in pure C99.
+
+**THE PROJECT IS STILL UNDER DEVELOPMENT AND MAY BE UNSTABLE**
+
+Remaining task:
+- Unregister system
+
+## Example of Usage
+
+Include the right headers
+```C
+#include "ecs/ecs.h"
+#include "ecs/ecs_err.h"
+```
+
+Declare components.
+```C
+typedef struct
+{
+    float x, y, z;
+} transform_t;
+
+typedef struct
+{
+    float vx, vy, vz;
+} rigidbody_t;
+```
+
+Define systems.
+```C
+ecs_err_t physics_system(ecs_entity_t *it, int count, void *args)
+{
+    float dt = ((float *)args)[0];
+
+    for (int i = 0; i < count; ++i)
+    {
+        transform_t *transform;
+        rigidbody_t *rb;
+        ecs_get_component(it[i], transform_t, &transform);
+        ecs_get_component(it[i], rigidbody_t, &rb);
+
+        transform->x += rb->vx * dt;
+        ...
+    }
+
+    return ECS_OK;
+}
+```
+
+Before all, initialize the library with `ecs_init()`. Then, create a scene. Each scene has its own components, entities and systems.
+```C
+ecs_scene_t scene;
+ecs_create_scene(&scene);
+ecs_bind_scene(scene);
+
+ecs_register_component(transform_t);
+ecs_register_component(rigidbody_t);
+
+ecs_signature_t signature;
+ecs_create_signature(&signature, transform_t, rigidbody_t);
+ecs_register_system(physics_system, ECS_SYSTEM_ON_UPDATE, signature);
+
+float dt = 0.1f;
+ecs_set_system_parameters(physics_system, &dt);
+
+ecs_entity_t player;
+ecs_create_entity(&player);
+ecs_add_component(player, transform_t, &((transform_t){ .x=1, .y=2, .z=3 }));
+ecs_add_component(player, rigidbody_t, &((rigidbody_t){ .vx=1 }));
+```
+
+You can retrieve back your entity's components with:
+```C
+transform_t *transform;
+rigidbody_t *rb;
+ecs_get_component(player, transform_t, &transform);
+ecs_get_component(player, rigidbody_t, &rb);
+```
+
+Call systems that have `ECS_SYSTEM_ON_UPDATE` event in the gameloop.
+```C
+while (1)
+{
+    ecs_listen_systems(ECS_SYSTEM_ON_UPDATE);
+    ecs_get_system_status(physics_system, &ret);
+    ECS_CHECK_ERROR(ret, "Error in the system : ");
+
+    printf("Transform : x=%.2f, y=%.2f, z=%.2f\n",
+            transform->x, transform->y, transform->z);
+}
+```
+
+Free the memory with `ecs_terminate()`
+
+## License
+
+This project is licensed under the MIT License. See the full license text [here](./LICENSE).tity-Component-System in pure C99.
--- example/main.c
+++ example/main.c
@@ -38,55 +38,47 @@ ecs_err_t physics_system(ecs_entity_t *it, int count, void *args)
 
 int main(void)
 {
-    ecs_init();
-
     ecs_err_t ret;
+
+    ecs_init(); // Has to be done first
 
     ecs_scene_t scene;
     ecs_create_scene(&scene);
-    ecs_bind_scene(scene);
+    ecs_bind_scene(scene); // Has to be done before any operation
 
+    ecs_register_component(transform_t);
+    ecs_register_component(rigidbody_t);
+    
     ecs_entity_t player;
     ecs_create_entity(&player);
+    ecs_add_component(player, transform_t, &((transform_t){ .x=1, .y=2, .z=3 }));
+    ecs_add_component(player, rigidbody_t, &((rigidbody_t){ .vx=1 }));
 
-    ret = ecs_register_component(transform_t);
-    ret |= ecs_register_component(rigidbody_t);
-    ECS_CHECK_ERROR(ret, "Failed to register components : ");
-    
-    ret |= ecs_add_component(player, transform_t, &((transform_t){ .x=1, .y=2, .z=3 }));
-    ret |= ecs_add_component(player, rigidbody_t, &((rigidbody_t){ .vx=1 }));
-    ECS_CHECK_ERROR(ret, "Failed to add components : ");
+    // Create physic system
+    ecs_signature_t signature;
+    ecs_create_signature(&signature, transform_t, rigidbody_t);
+    ecs_register_system(physics_system, ECS_SYSTEM_ON_UPDATE, signature);
 
+    // Get entity components
     transform_t *transform;
     rigidbody_t *rb;
-    ret |= ecs_get_component(player, transform_t, &transform);
+    ret = ecs_get_component(player, transform_t, &transform);
     ret |= ecs_get_component(player, rigidbody_t, &rb);
     ECS_CHECK_ERROR(ret, "Failed to get components : ");
 
-    printf("Transform : x=%.2f, y=%.2f, z=%.2f\n", transform->x, transform->y, transform->z);
-    printf("Rigidbody: vx=%.2f, vy=%.2f, vz=%.2f\n", rb->vx, rb->vy, rb->vz);
-
-    ecs_signature_t signature;
-    ret |= ecs_create_signature(&signature, transform_t, rigidbody_t);
-    ECS_CHECK_ERROR(ret, "Failed to create signature : ");
-
-    ret |= ecs_register_system(physics_system, ECS_SYSTEM_ON_UPDATE, signature);
-    ECS_CHECK_ERROR(ret, "Failed to register physics system : ");
-
-    float dt = 0.1f;
-    ecs_set_system_parameters(physics_system, &dt);
-
     ecs_listen_systems(ECS_SYSTEM_ON_INIT);
 
-    /* ecs_delete_entity(player); */
-
-    for (int i = 0; i < 10; ++i)
+    // Gameloop
+    float dt = 0.1f;
+    ecs_set_system_parameters(physics_system, &dt);
+    while (1)
     {
         ecs_listen_systems(ECS_SYSTEM_ON_UPDATE);
         ecs_get_system_status(physics_system, &ret);
         ECS_CHECK_ERROR(ret, "Error in the system : ");
 
-        printf("Transform : x=%.2f, y=%.2f, z=%.2f\n", transform->x, transform->y, transform->z);
+        printf("Transform : x=%.2f, y=%.2f, z=%.2f\n",
+                transform->x, transform->y, transform->z);
     }
 
     ecs_listen_systems(ECS_SYSTEM_ON_END);
@@ -94,6 +86,7 @@ int main(void)
     ecs_free_scene();
     ecs_bind_scene(0);
 
-    ecs_terminate();
+    ecs_terminate(); // Free memory
+
     return 0;
 }
